6972b5713dfa9038dddbb20760ffddaa019ac60e
! ¥S+$V • yxzhin :¥XZ:teamRU <134784079+yxzhin@users.noreply.github.com>
2023-09-14 20:07:47 +0200

    Add files via upload

---
+++ src/gd_2.1/backend/latest/yxzcore/config/connection.php
@@ -0,0 +1,14 @@
+
+
+
+<?php
+
+// last update: 04.09.2023.
+
+$host = "localhost";
+$port = 3306;
+$username = "";
+$dbname = "";
+$password = "";
+
+?>
---
+++ src/gd_2.1/backend/latest/yxzcore/config/security.php
@@ -0,0 +1,19 @@
+
+
+
+<?php
+
+// last update: 16:02 05.09.2023.
+
+$encryptionKey = "yxzgdps";
+
+$accountsPerIPLimit = 1;
+$songReuploadsPerIPLimit = 2;
+
+$hCaptchaEnabled = true;
+$hCaptchaKey = "";
+$hCaptchaSecret = "";
+
+//@TODO
+
+?>
